bda9c82f10ffd57d477dafa6332c5ce8acf94d8d
Gaurav-Dewangan-GD <dewanganjeetu83@gmail.com>
2022-12-23 15:34:05 +0530

    uncommentted gcdofnum

--- GCD and LCM with Problem/gcdofnum.py
+++ GCD and LCM with Problem/gcdofnum.py
@@ -1,13 +1,11 @@
 
+import time
+start_time = time.time()
+def gcd(a,b):
 
-# algo
-# import time
-# start_time = time.time()
-# def gcd(a,b):
-
-#     for i in range(min(a,b)+1,1,-1):  
-#         if (a%i) == 0  and (b%i) == 0:
-#             return i
-#     return(1) 
-# print(gcd(12,18))
-# print((time.time()-start_time))
+    for i in range(min(a,b)+1,1,-1):  
+        if (a%i) == 0  and (b%i) == 0:
+            return i
+    return(1) 
+print(gcd(12,18))
+print((time.time()-start_time))
